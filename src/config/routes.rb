3d92f0f6385bbd652d677a56af70d2b565026afd
makoto <junjuwarjunjuwar@gmail.com>
2022-05-28 18:17:59 +0900

    フォーメーション投稿用のユニフォーム追加

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'post_images#index'
   get 'posts/index'
   get 'posts/show'
   get 'posts/new'
@@ -7,6 +8,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
   resources :posts
-
 
 end
