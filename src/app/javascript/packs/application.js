d57defa44130a284d75216f593f5dbd89e95467d
makoto <junjuwarjunjuwar@gmail.com>
2022-05-29 22:13:05 +0900

    フォーメーション投稿画面作成

--- src/app/javascript/packs/application.js
+++ src/app/javascript/packs/application.js
@@ -3,11 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// 5月29日追加jQueryの導入
+require("jquery")
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// 5月29日追加jQueryの導入
+import 'bootstrap'
+import '../src/application.scss'
