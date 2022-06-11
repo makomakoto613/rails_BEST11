106cf41fdb61db73dab0512b48b7072efbeaef68
makoto <junjuwarjunjuwar@gmail.com>
2022-06-11 21:17:49 +0900

    webpackでcss適用

--- src/app/javascript/packs/application.js
+++ src/app/javascript/packs/application.js
@@ -3,12 +3,19 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
+import "../src/style.scss";
+
+require("jquery");
+require("bootstrap");
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import '../stylesheets/material-dashboard.css';
+
 
 Rails.start()
 Turbolinks.start()
