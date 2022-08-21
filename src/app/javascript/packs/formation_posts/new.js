65ea1dcc80e616976baaac1e698d17933f5ee4b5
makoto <junjuwarjunjuwar@gmail.com>
2022-08-21 13:29:42 +0900

    編集画面修正

--- src/app/javascript/packs/formation_posts/new.js
+++ src/app/javascript/packs/formation_posts/new.js
@@ -1,137 +1,138 @@
-<script>
-  window.onload = function () {
-        // canvas要素を取得
-        var canvas = document.getElementById("canvas");
-  // canvas要素が持つgetContext()メソッドを実行し、
-  // グラフィック描画のためのメソッドやプロパティを
-  // 持つオブジェクトを取得する。
-  // 引数を"2d"とすることで2Dグラフィックの描画に
-  // 特化したメソッドやプロパティを持つオブジェクトを取得し、
-  // 定数ctxに格納する。
-  var ctx = canvas.getContext("2d");
-  // 定数ctxに格納したオブジェクトがもつプロパティやメソッドは
-  // ctx.プロパティ名、ctx.メソッド名() で呼び出せる
+    window.onload = function () {
+      // canvas要素を取得
+      var canvas = document.getElementById("canvas");
+      // canvas要素が持つgetContext()メソッドを実行し、
+      // グラフィック描画のためのメソッドやプロパティを
+      // 持つオブジェクトを取得する。
+      // 引数を"2d"とすることで2Dグラフィックの描画に
+      // 特化したメソッドやプロパティを持つオブジェクトを取得し、
+      // 定数ctxに格納する。
+      var ctx = canvas.getContext("2d");
+      // 定数ctxに格納したオブジェクトがもつプロパティやメソッドは
+      // ctx.プロパティ名、ctx.メソッド名() で呼び出せる
 
 
-  var BB = canvas.getBoundingClientRect();
-  //左辺の座標
-  var offsetX = BB.left;
-  //四角形の上辺の座標
-  var offsetY = BB.top;
-  var WIDTH = canvas.width;
-  var HEIGHT = canvas.height;
+      var BB = canvas.getBoundingClientRect();
+      //左辺の座標
+      var offsetX = BB.left;
+      //四角形の上辺の座標
+      var offsetY = BB.top;
+      var WIDTH = canvas.width;
+      var HEIGHT = canvas.height;
 
-  var dragok = false;
-  var startX;
-  var startY;
+      var dragok = false;
+      var startX;
+      var startY;
 
-  var img = new Image();
-  img.src = "/images/uni.jpg";
-        img.onload = () => {
-    draw();
-        }
-  var coordinates = document.getElementById('coordinates').value;
-
-  var rects = JSON.parse(coordinates);
-
-  draw();
-
-  function rect(x, y, w, h) {
-    ctx.beginPath();
-  ctx.rect(x, y, w, h);
-  ctx.closePath();
-  ctx.fill();
-    }
-  //アニメーションの各フレームの開始時に必要
-  function clear() {
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    }
-
-  function draw() {
-    clear();
-  ctx.fillStyle = "#FAF7F8";
-  rect(0, 0, WIDTH, HEIGHT);
-
-  for (var i = 0; i < rects.length; i++) {
-        var r = rects[i];
-  ctx.fillStyle = r.fill;
-
-  //四角の場合
-  //rect(r.x, r.y, r.width, r.height);
-
-  //アイコンフォントの場合
-  //ctx.font = "900 " + r.width + "px 'Font Awesome 5 Free'";
-  //ctx.fillText("\uf553", r.x - 3, r.y + r.height - 2);
-
-  //画像の場合
-  ctx.drawImage(img, r.x, r.y, r.width, r.height);
+      var img = new Image();
+      img.src = "/images/uni.jpg";
+      img.onload = () => {
+        draw();
       }
 
-    }
-  canvas.onmousedown = myDown;
-  function myDown(e) {
-    e.preventDefault();
-  e.stopPropagation();
-  //e.clientXはWEB画面全体のx座標（押した箇所のx座標）
-  //offsetXはcanvasの左上のx座標
-  var mx = parseInt(e.clientX - offsetX);
-  var my = parseInt(e.clientY - offsetY);
-  //アイコンを押したときのx座標とy座標を記録している
-  dragok = false;
-  for (var i = 0; i < rects.length; i++) {
-        var r = rects[i];
-        if (mx > r.x && mx < r.x + r.width && my > r.y && my < r.y + r.height) {
-    dragok = true;
-  r.isDragging = true;
-        }
+      var coordinates = document.getElementById('formation_post_coordinates').value;
+
+      var rects = JSON.parse(formation_post_coordinates);
+
+      draw();
+
+      function rect(x, y, w, h) {
+        ctx.beginPath();
+        ctx.rect(x, y, w, h);
+        ctx.closePath();
+        ctx.fill();
       }
-  //startX,startYに最初アイコンをクリックした時の座標が記録される
-  startX = mx;
-  startY = my;
-    }
+      //アニメーションの各フレームの開始時に必要
+      function clear() {
+        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+      }
 
-  canvas.onmouseup = myUp;
-  function myUp(e) {
-    e.preventDefault();
-  e.stopPropagation();
+      function draw() {
+        clear();
+        ctx.fillStyle = "#FAF7F8";
+        rect(0, 0, WIDTH, HEIGHT);
 
-  dragok = false;
-  for (var i = 0; i < rects.length; i++) {
-    rects[i].isDragging = false;
-        }
-  document.getElementById('formation_post_coordinates').value = JSON.stringify(rects);
-
-    }
-
-  canvas.onmousemove = myMove;
-  function myMove(e) {
-      if (dragok) {
-    e.preventDefault();
-  e.stopPropagation();
-
-  var mx = parseInt(e.clientX - offsetX);
-  var my = parseInt(e.clientY - offsetY);
-
-  var dx = mx - startX;
-  var dy = my - startY;
-
-  for (var i = 0; i < rects.length; i++) {
+        for (var i = 0; i < rects.length; i++) {
           var r = rects[i];
-  if (r.isDragging) {
-    r.x += dx;
-  r.y += dy;
+          ctx.fillStyle = r.fill;
 
+          //四角の場合
+          //rect(r.x, r.y, r.width, r.height);
+
+          //アイコンフォントの場合
+          //ctx.font = "900 " + r.width + "px 'Font Awesome 5 Free'";
+          //ctx.fillText("\uf553", r.x - 3, r.y + r.height - 2);
+
+          //画像の場合
+          ctx.drawImage(img, r.x, r.y, r.width, r.height);
+        }
+
+      }
+      canvas.onmousedown = myDown;
+
+      function myDown(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        //e.clientXはWEB画面全体のx座標（押した箇所のx座標）
+        //offsetXはcanvasの左上のx座標
+        var mx = parseInt(e.clientX - offsetX);
+        var my = parseInt(e.clientY - offsetY);
+        //アイコンを押したときのx座標とy座標を記録している
+        dragok = false;
+        for (var i = 0; i < rects.length; i++) {
+          var r = rects[i];
+          if (mx > r.x && mx < r.x + r.width && my > r.y && my < r.y + r.height) {
+            dragok = true;
+            r.isDragging = true;
           }
         }
+        //startX,startYに最初アイコンをクリックした時の座標が記録される
+        startX = mx;
+        startY = my;
+      }
 
-  draw();
+      canvas.onmouseup = myUp;
 
-  startX = mx;
-  startY = my;
+      function myUp(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        dragok = false;
+        for (var i = 0; i < rects.length; i++) {
+          rects[i].isDragging = false;
+        }
+        document.getElementById('formation_post_coordinates').value = JSON.stringify(rects);
 
       }
-  }
+
+      canvas.onmousemove = myMove;
+
+      function myMove(e) {
+        if (dragok) {
+          e.preventDefault();
+          e.stopPropagation();
+
+          var mx = parseInt(e.clientX - offsetX);
+          var my = parseInt(e.clientY - offsetY);
+
+          var dx = mx - startX;
+          var dy = my - startY;
+
+          for (var i = 0; i < rects.length; i++) {
+            var r = rects[i];
+            if (r.isDragging) {
+              r.x += dx;
+              r.y += dy;
+
+            }
+          }
+
+          draw();
+
+          startX = mx;
+          startY = my;
+
+        }
+      }
 
     };
-
-</script>
